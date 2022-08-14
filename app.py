c572be79886d01070c0b75c97e6276136f247349
sid00100 <otasiddhant@gmail.com>
2022-08-14 23:18:17 +0530

    directories restructured

--- app.py
+++ app.py
@@ -5,4 +5,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/signup')
+def form():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 app.run(debug=True)
